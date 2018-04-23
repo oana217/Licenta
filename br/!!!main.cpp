585013bd63b3c255a015bf31bfb9b1a7f846ae29
oana217 <30893715+oana217@users.noreply.github.com>
2018-04-23 16:52:43 +0300

    decision based on multiple frames

--- br/!!!main.cpp
+++ br/!!!main.cpp
@@ -1,3 +1,5 @@
+#define WINVER 0x0500
+#include <windows.h>
 #include "!!!image.h"
 #include "!!!processing.h"
 #include "!!!drawing.h"
@@ -6,14 +8,13 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
-
 using namespace cv;
 using namespace std;
 
 int main() {
 	int count = 0;
 	int prevGestureIndex = 0;
-
+	int validGestureIndex = 0;
 	VideoCapture camera(0);
 	if (!camera.isOpened()) {
 		cout << "Could not open camera." << endl;
@@ -24,6 +25,26 @@ int main() {
 	img.SetCamera(camera);
 
 	while (true) {
+		/*INPUT ip;
+
+		// Pause for 5 seconds.
+		Sleep(1000);
+
+		// Set up a generic keyboard event.
+		ip.type = INPUT_KEYBOARD;
+		ip.ki.wScan = 0; // hardware scan code for key
+		ip.ki.time = 0;
+		ip.ki.dwExtraInfo = 0;
+
+		// Press the "UP" key
+		ip.ki.wVk = 0x26; // virtual-key code for the "UP" key
+		ip.ki.dwFlags = 0; // 0 for key press
+		SendInput(1, &ip, sizeof(INPUT));
+
+		// Release the "A" key
+		ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+		SendInput(1, &ip, sizeof(INPUT));*/
+
 		img.Setup();
 		Mat img_roi = img.GetROI();
 		Mat img_converted = img.GetConvertedImage(); //these are just for imshow
@@ -40,17 +61,21 @@ int main() {
 		if (gestureIndex >= 1) {
 			if (gestureIndex == prevGestureIndex) {
 				count++;
-				img_draw.DrawGesture(gestureIndex);
+				//img_draw.DrawGesture(gestureIndex);
+			}
+			else {
+				count = 0;
 			}
 			prevGestureIndex = gestureIndex;
 
 		}
-		if (count == 5) {
-			img_draw.SetGestureIndex(gestureIndex);
-			img_draw.DrawGesture(gestureIndex);
+		if (count == 10) {
+			//img_draw.SetGestureIndex(gestureIndex);
+			validGestureIndex = gestureIndex;
 			count = 0;
 		}
-
+		img_draw.DrawGesture(validGestureIndex);
+		//validGestureIndex = 0;
 		img_draw.DrawData();
 		Mat drw = img_draw.GetDrawing();
 
